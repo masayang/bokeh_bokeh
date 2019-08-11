c65886b3b333741b0af74e260d83b9234096bf12
masayang <masayang.mobile@gmail.com>
2019-08-11 15:38:06 -0700

    level3 Candlestick

--- samples/level_3/candlestick.py
+++ samples/level_3/candlestick.py
@@ -2,6 +2,7 @@ import pandas as pd
 from bokeh.plotting import figure, show, output_file
 from math import pi
 from bokeh.models import HoverTool
+from bokeh.layouts import gridplot
 
 
 def read_data():
@@ -28,11 +29,19 @@ def candlestick(range=100):
         x_range=(df.iloc[-1*range].Date, df.iloc[-1].Date),
         y_range=(df.iloc[-1*range:].Low.min(), df.iloc[-1*range:].High.max())
     )
+
+
     plot.xaxis.major_label_orientation = pi/4
-    plot.grid.grid_line_alpha=0.3
+#    plot.grid.grid_line_alpha=0.3
     plot.segment(df.Date, df.High, df.Date, df.Low, color="black")
     plot.vbar(x='Date', width=w, bottom='Open', top='Close', fill_color="#D5E1DD", line_color="black", source=df[inc])
     plot.vbar(x='Date', width=w, top='Open', bottom='Close', fill_color="#F2583E", line_color="black", source=df[dec])
+    plot.xaxis.visible = False
+
+    plot2 = figure(plot_width=1000, plot_height=125, x_range=plot.x_range, y_range=(0, df.iloc[-1*range:].Volume.max()), title=None)
+    plot2.vbar(x='Date', width=w, bottom=0, top='Volume', fill_color="#D5E1DD", line_color="black", source=df[inc])
+    plot2.vbar(x='Date', width=w, top='Volume', bottom=0, fill_color="#F2583E", line_color="black", source=df[dec])
+    
     hover_tools = HoverTool(
         tooltips=[
             ("Date", "@Date{%F}"),
@@ -46,7 +55,7 @@ def candlestick(range=100):
     )
     plot.add_tools(hover_tools)
     output_file("bokeh.html", title="Candle Stick Example")
-    return plot
+    return gridplot([[plot], [plot2]])
     
 
 
